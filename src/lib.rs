1f929080b9448ee14ef43a82521a0aa49f120b84
bsoptei <bsoptei@gmail.com>
2021-03-07 20:56:51 +0100

    test all endpoints, standardize

--- src/lib.rs
+++ src/lib.rs
@@ -24,18 +24,21 @@ pub struct StoreProvider;
 #[async_trait]
 pub trait DataStore {
     async fn add(&self, item: Item) -> Option<Key>;
-    async fn delete(&self, key: Key) -> Option<String>;
+    async fn delete(&self, key: Key) -> Option<Key>;
     async fn get(&self, key: Key) -> String;
     async fn get_all(&self) -> String;
     async fn replace(&self, key: Key, item: Item) -> Option<Key>;
     async fn update(&self, key: Key, item: Item) -> Option<Key>;
 }
 
+pub const DATA_ENDPOINT_PATTERN: &str = "/data";
+pub const DATA_ENDPOINT_KEY_PATTERN: &str = "/data/{key}";
+
 pub fn configure_endpoints<Store: 'static + DataStore>(cfg: &mut web::ServiceConfig) {
-    cfg.route("/data", web::post().to(create::<Store>))
-        .route("/data/{key}", web::delete().to(delete::<Store>))
-        .route("/data/{key}", web::get().to(read::<Store>))
-        .route("/data", web::get().to(read_all::<Store>))
-        .route("/data/{key}", web::put().to(replace::<Store>))
-        .route("/data/{key}", web::patch().to(update::<Store>));
+    cfg.route(DATA_ENDPOINT_PATTERN, web::post().to(create::<Store>))
+        .route(DATA_ENDPOINT_KEY_PATTERN, web::delete().to(delete::<Store>))
+        .route(DATA_ENDPOINT_KEY_PATTERN, web::get().to(read::<Store>))
+        .route(DATA_ENDPOINT_PATTERN, web::get().to(read_all::<Store>))
+        .route(DATA_ENDPOINT_KEY_PATTERN, web::put().to(replace::<Store>))
+        .route(DATA_ENDPOINT_KEY_PATTERN, web::patch().to(update::<Store>));
 }
